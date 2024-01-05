73662a36823c053ce7b42227ca04cdde1ebec7bf
RyanRonish <roni0210@bears.unco.edu>
2024-01-05 16:06:20 -0700

    keras install and nna

--- priceprediction.py
+++ priceprediction.py
@@ -21,6 +21,8 @@ data.reset_index(drop=True, inplace=True)
 
 print(data.tail())
 
+#candlestick graph on browser
+
 import plotly.graph_objects as go
 
 figure = go.Figure(data=[go.Candlestick(x=data["Date"],
@@ -32,5 +34,37 @@ figure.update_layout(title = "Stock Price Analysis",
                      xaxis_rangeslider_visible=False)
 figure.show()
 
+# correlation data
 correlation = data.corr()
 print(correlation["Close"].sort_values(ascending=False))
+
+#LSTM
+
+x = data[["Open", "High", "Low", "Volume"]]
+y = data["Close"]
+x = x.to_numpy()
+y = y.to_numpy()
+y = y.reshape(-1, 1)
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# neural network architecture
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape= (xtrain.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+model.summary()
+
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(xtrain, ytrain, batch_size=1, epochs=30)
+
+
+import numpy as np
+#features = [Open, High, Low, Adj Close, Volume]
+features = np.array([[177.089996, 180.419998, 177.070007, 74919600]])
+model.predict(features)
